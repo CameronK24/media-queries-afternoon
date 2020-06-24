396f56a848483df723a3f1f746c8978d93b9f9a7
CameronK24 <cskelly15@gmail.com>
2020-06-24 16:52:03 -0600

    a little more to do i think

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import './reset.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      dropdownView: false,
+    }
+  }
+
+  toggleDropdownMenu = () => {
+    this.setState({dropdownView: !this.state.dropdownView}) 
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <div className='nav-header'>
+            <p>Start Bootstrap</p>
+            <nav className='nav-bar'>
+              <span>About</span>
+              <span>Projects</span>
+              <span>Contact</span>
+            </nav>
+            <div className='dropdown-menu' onClick={this.toggleDropdownMenu}>Menu
+            <div className='menu-line-location'>
+              <div className='menu-line'></div>
+              <div className='menu-line'></div>
+              <div className='menu-line'></div>
+            </div>
+            </div>         
+          </div>
+          {this.state.dropdownView
+              ? (
+                <nav className='mobile-nav-menu'>
+                  <span>About</span>
+                  <span>Projects</span>
+                  <span>Contact</span>
+                </nav>
+              )
+              : null
+            }
+        </header>
+        <section className='section-1'>
+
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
